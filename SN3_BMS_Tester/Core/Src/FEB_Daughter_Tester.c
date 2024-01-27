9288f9d382e20f6fadbabb0235dc0d5eb1fd16a7
Andy Yang <ayangsea@gmail.com>
2024-01-26 17:23:48 -0800

    update names for constants

--- SN3_BMS_Tester/Core/Src/FEB_Daughter_Tester.c
+++ SN3_BMS_Tester/Core/Src/FEB_Daughter_Tester.c
@@ -19,13 +19,13 @@ void FEB_Daughter_Tester_Init(void) {
 		//voltage
 		FEB_BMS_Tester_Hardware_Set_DAC_CS_n(cell, false); //CS low
 		HAL_Delay(5); //Delay 5ms
-		FEB_Input_Voltages_Input_Cell_Voltage(cell, CELL_DEFAULT_VOLTAGE);
+		FEB_Input_Voltages_Input_Cell_Voltage(cell, FEB_CONSTANT_CELL_DEFAULT_VOLTAGE);
 		HAL_Delay(5); //Delay 5ms
 		FEB_BMS_Tester_Hardware_Set_DAC_CS_n(cell, true); //CS high
 
 		//temperature
 		FEB_BMS_Tester_Hardware_Configure_MUX(cell);
-		FEB_Input_Voltages_Input_Temp_Voltage(TEMP_DEFAULT_VOLTAGE);
+		FEB_Input_Voltages_Input_Temp_Voltage(FEB_CONSTANT_TEMP_DEFAULT_VOLTAGE);
 	}
 }
 
@@ -33,8 +33,8 @@ void FEB_Daughter_Tester_Init(void) {
 
 void FEB_Daughter_Tester_Test_Cell_Voltages(void) {
 
-	for (uint8_t curr_cell = 0; curr_cell < NUM_CELLS; curr_cell++) {
-		for (float voltage = CELL_MIN_VOLTAGE; voltage <= CELL_MAX_VOLTAGE; voltage += 0.1) {
+	for (uint8_t curr_cell = 0; curr_cell < FEB_CONSTANT_NUM_CELLS; curr_cell++) {
+		for (float voltage = FEB_CONSTANT_CELL_MIN_VOLTAGE; voltage <= FEB_CONSTANT_CELL_MAX_VOLTAGE; voltage += 0.1) {
 				FEB_BMS_Tester_Hardware_Set_DAC_CS_n(curr_cell, false); //CS low
 				HAL_Delay(5); //Delay 5ms
 				FEB_Input_Voltages_Input_Cell_Voltage(curr_cell, voltage);
@@ -42,11 +42,11 @@ void FEB_Daughter_Tester_Test_Cell_Voltages(void) {
 				FEB_BMS_Tester_Hardware_Set_DAC_CS_n(curr_cell, true); //CS high
 				FEB_LTC6811_Poll_Voltage(); //Poll voltage
 				FEB_LTC6811_Poll_Temperature(); //Poll temperature
-				FEB_Validate_Readings_Validate_Voltages(1, voltage, curr_cell); //IC 1 for daughter board
+				FEB_Validate_Readings_Validate_Voltages(FEB_CONSTANT_DAUGHTER_TESTER_IC, voltage, curr_cell); //IC for daughter board
 			}
 			FEB_BMS_Tester_Hardware_Set_DAC_CS_n(curr_cell, false); //CS low
 			HAL_Delay(5); //Delay 5ms
-			FEB_Input_Voltages_Input_Cell_Voltage(curr_cell, CELL_DEFAULT_VOLTAGE); //Reset Cell Voltage
+			FEB_Input_Voltages_Input_Cell_Voltage(curr_cell, FEB_CONSTANT_CELL_DEFAULT_VOLTAGE); //Reset Cell Voltage
 			HAL_Delay(5); //Delay 5ms
 			FEB_BMS_Tester_Hardware_Set_DAC_CS_n(curr_cell, true); //CS high
 	}
@@ -56,15 +56,15 @@ void FEB_Daughter_Tester_Test_Cell_Voltages(void) {
 // ******************************** Test Temperatures ********************************
 
 void FEB_Daughter_Tester_Test_Cell_Temps(void) {
-	for (uint8_t cell = 0; cell < NUM_CELLS; cell++) {
+	for (uint8_t cell = 0; cell < FEB_CONSTANT_NUM_CELLS; cell++) {
 		FEB_BMS_Tester_Hardware_Configure_MUX(cell);
-		for (float temp_voltage = CELL_MIN_TEMP_VOLTAGE; temp_voltage <= CELL_MAX_TEMP_VOLTAGE; temp_voltage += 0.05) { //TODO: Figure out how much to increment by
+		for (float temp_voltage = FEB_CONSTANT_CELL_MIN_TEMP_VOLTAGE; temp_voltage <= FEB_CONSTANT_CELL_MAX_TEMP_VOLTAGE; temp_voltage += 0.05) { //TODO: Figure out how much to increment by
 			FEB_Input_Voltages_Input_Temp_Voltage(temp_voltage);
 			FEB_LTC6811_Poll_Temperature(); //Poll voltage
 			FEB_LTC6811_Poll_Temperature(); //Poll temperature
-			FEB_Validate_Readings_Validate_Temperatures(1, temp_voltage); //IC 1 for daughter board
+			FEB_Validate_Readings_Validate_Temperatures(FEB_CONSTANT_DAUGHTER_TESTER_IC, temp_voltage); //IC for daughter board
 		}
-		FEB_Input_Voltages_Input_Temp_Voltage(TEMP_DEFAULT_VOLTAGE);
+		FEB_Input_Voltages_Input_Temp_Voltage(FEB_CONSTANT_TEMP_DEFAULT_VOLTAGE);
 	}
 }
 
--- SN3_BMS_Tester/Core/Src/FEB_Validate_Readings.c
+++ SN3_BMS_Tester/Core/Src/FEB_Validate_Readings.c
@@ -24,19 +24,19 @@ void FEB_Validate_Readings_Validate_Voltages(uint8_t ic, float expected_voltage,
 	for (uint8_t cell = 0; cell < CELLS_TO_TEST; cell++) {
 		float polled_voltage = FEB_LTC6811_Get_Voltage(ic, cell);
 		if (curr_cell == cell) {
-			float voltage_threshold_high = expected_voltage + CELL_VOLTAGE_THRESHOLD_mV * 0.001;
-			float voltage_threshold_low = expected_voltage - CELL_VOLTAGE_THRESHOLD_mV * 0.001;
+			float voltage_threshold_high = expected_voltage + FEB_CONSTANT_CELL_VOLTAGE_THRESHOLD_mV * 0.001;
+			float voltage_threshold_low = expected_voltage - FEB_CONSTANT_CELL_VOLTAGE_THRESHOLD_mV * 0.001;
 			if (polled_voltage > voltage_threshold_high || polled_voltage < voltage_threshold_low) {
 				char errmsg[256];
 				sprintf(errmsg, "Cell %d. Expected voltage: %f, actual voltage: %f\n", cell, expected_voltage, polled_voltage); //TODO: add which ic is being read from
 				HAL_UART_Transmit(&huart2, (uint8_t*) errmsg, strlen(errmsg), 100);
 			}
 		} else {
-			float voltage_threshold_high = CELL_DEFAULT_VOLTAGE + CELL_VOLTAGE_THRESHOLD_mV * 0.001;
-			float voltage_threshold_low = CELL_DEFAULT_VOLTAGE - CELL_VOLTAGE_THRESHOLD_mV * 0.001;
+			float voltage_threshold_high = FEB_CONSTANT_CELL_DEFAULT_VOLTAGE + FEB_CONSTANT_CELL_VOLTAGE_THRESHOLD_mV * 0.001;
+			float voltage_threshold_low = FEB_CONSTANT_CELL_DEFAULT_VOLTAGE - FEB_CONSTANT_CELL_VOLTAGE_THRESHOLD_mV * 0.001;
 			if (polled_voltage > voltage_threshold_high || polled_voltage < voltage_threshold_low) {
 				char errmsg[256];
-				sprintf(errmsg, "Cell %d. Expected voltage: %f, actual voltage: %f\n", cell, CELL_DEFAULT_VOLTAGE, polled_voltage);
+				sprintf(errmsg, "Cell %d. Expected voltage: %f, actual voltage: %f\n", cell, FEB_CONSTANT_CELL_DEFAULT_VOLTAGE, polled_voltage);
 				HAL_UART_Transmit(&huart2, (uint8_t*) errmsg, strlen(errmsg), 100);
 			}
 		}
@@ -48,7 +48,7 @@ void FEB_Validate_Readings_Validate_Voltages(uint8_t ic, float expected_voltage,
 void FEB_Validate_Readings_Validate_Temperatures(uint8_t ic, float expected_temperature_voltage) {
 	for (uint8_t cell = 0; cell < CELLS_TO_TEST; cell++) {
 		float polled_temperature_voltage = FEB_LTC6811_Get_Temperature_Voltage(ic, cell);
-		if (polled_temperature_voltage > expected_temperature_voltage + CELL_TEMP_THRESHOLD_mV * 0.0001 || polled_temperature_voltage < expected_temperature_voltage - CELL_TEMP_THRESHOLD_mV * 0.0001) {
+		if (polled_temperature_voltage > expected_temperature_voltage + FEB_CONSTANT_CELL_TEMP_THRESHOLD_mV * 0.0001 || polled_temperature_voltage < expected_temperature_voltage - FEB_CONSTANT_CELL_TEMP_THRESHOLD_mV * 0.0001) {
 			char errmsg[256];
 			sprintf(errmsg, "Cell %d. Expected temp voltage: %f, actual temp voltage: %f\n", cell, expected_temperature_voltage, polled_temperature_voltage);
 			HAL_UART_Transmit(&huart2, (uint8_t*) errmsg, strlen(errmsg), 100);
