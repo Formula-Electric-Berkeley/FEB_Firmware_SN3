bd0e6a5d111f9d752ce4a5f13d4d1c0a30c95c8f
Viplove Rahate <viploverahate@wifi-10-44-244-187.wifi.berkeley.edu>
2024-03-03 15:20:04 -0800

    added button/switch functionality for button 4 and switches 1-3

--- SWDB2023/Core/Src/FEB_Buttons.c
+++ SWDB2023/Core/Src/FEB_Buttons.c
@@ -11,20 +11,40 @@ static bool Button_Checking = false;
 static bool Button_Timer_Flag = false;
 
 //coolant pump
-bool lastButton_4 = 0;
-bool lock_4 = 0;
+bool lastButton_1 = 0;
+bool lock_1 = 0;
 bool coolant_pump = 0;
 
 
 //accumulator fans
-bool lastButton_5 = 0;
-bool lock_5 = 0;
+bool lastButton_2 = 0;
+bool lock_2 = 0;
 bool accumulator_fans = 0;
 
-//extra
-bool lastButton_6 = 0;
+//extra_1
+bool lastButton_3 = 0;
+bool lock_3 = 0;
+bool extra_1 = 0;
+
+//extra_2
+bool lastButton_4 = 0;
+bool lock_4 = 0;
+bool extra_2 = 0;
+
+//switch_1
+bool lastSwitch_1 = 0;
+bool lock_5 = 0;
+bool switch_1 = 0;
+
+//switch_2
+bool lastSwitch_2 = 0;
 bool lock_6 = 0;
-bool extra = 0;
+bool switch_2 = 0;
+
+//switch_3
+bool lastSwitch_3 = 0;
+bool lock_7 = 0;
+bool switch_3 = 0;
 
 //ready to drive
 bool ready_to_drive = false;
@@ -34,11 +54,62 @@ bool lock = false;
 // ********************************** Functions **********************************
 
 FEB_Buttons_coolantPump(){
-	if (!(data2 & (1<<1))) { //BUT_4 --> IO2 P1, coolant pump
-		if (!lastButton_4){
-			lock_4 = true;
+	if (!(data2 & (1<<1))) { //BUT_1 --> IO P1, coolant pump
+		if (!lastButton_1){
+			lock_1 = true;
 			coolant_pump = !coolant_pump;
 			//FEB_CAN_Transmit(&hcan1, SW_COOLANT_PUMP, (uint8_t *) &coolant_pump, 1);
+		}
+		lastButton_1 = true;
+	} else {
+		if (lastButton_1){
+			lock_1 = false;
+		}
+		lastButton_1 = false;
+
+	}
+}
+
+FEB_Buttons_accumulatorFans(){
+	if(!(data2 & (1<<2))) { //BUT_2 -> IO P2, accumulator fans
+		if (!lastButton_2){
+			lock_2 = true;
+			accumulator_fans = !accumulator_fans;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &accumulator_fans, 1);
+		}
+		lastButton_2 = true;
+	} else {
+		if (lastButton_2){
+			lock_2 = false;
+		}
+		lastButton_2 = false;
+	}
+
+}
+
+FEB_Buttons_extra_1(){
+	if(!(data2 & (1<<3))) { //BUT_3 -> IO P3, extra_1
+		if (!lastButton_3){
+			lock_3 = true;
+			extra_1 = !extra_1;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra_1, 1);
+		}
+		lastButton_3 = true;
+	} else {
+		if (lastButton_3){
+			lock_3 = false;
+		}
+		lastButton_3 = false;
+	}
+
+}
+
+FEB_Buttons_extra_2(){
+	if(!(data2 & (1<<4))) { //BUT_4 -> IO P4, extra_2
+		if (!lastButton_4){
+			lock_4 = true;
+			extra_2 = !extra_2;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra_1, 1);
 		}
 		lastButton_4 = true;
 	} else {
@@ -46,46 +117,63 @@ FEB_Buttons_coolantPump(){
 			lock_4 = false;
 		}
 		lastButton_4 = false;
-
 	}
+
 }
 
-FEB_Buttons_accumulatorFans(){
-	if(!(data2 & (1<<2))) { //BUT_5 -> IO@ P2, accumulator fans
-		if (!lastButton_5){
+FEB_Buttons_switch_1(){
+	if(!(data2 & (1<<5))) { //SWITCH_1 -> IO P5, switch_1
+		if (!lastSwitch_1){
 			lock_5 = true;
-			accumulator_fans = !accumulator_fans;
-			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &accumulator_fans, 1);
+			switch_1 = !switch_1;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra_1, 1);
 		}
-		lastButton_5 = true;
+		lastSwitch_1 = true;
 	} else {
-		if (lastButton_5){
+		if (lastSwitch_1){
 			lock_5 = false;
 		}
-		lastButton_5 = false;
+		lastSwitch_1 = false;
 	}
 
 }
 
-FEB_Buttons_Extra(){
-	if(!(data2 & (1<<3))) { //BUT_6 -> IO@ P3, extra
-		if (!lastButton_6){
+FEB_Buttons_switch_2(){
+	if(!(data2 & (1<<6))) { //SWITCH_2 -> IO P6, switch_2
+		if (!lastSwitch_2){
 			lock_6 = true;
-			extra = !extra;
-			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra, 1);
+			switch_2 = !switch_2;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra_1, 1);
 		}
-		lastButton_6 = true;
+		lastSwitch_2 = true;
 	} else {
-		if (lastButton_6){
+		if (lastSwitch_2){
 			lock_6 = false;
 		}
-		lastButton_6 = false;
+		lastSwitch_2 = false;
+	}
+
+}
+
+FEB_Buttons_switch_3(){
+	if(!(data2 & (1<<7))) { //SWITCH_3 -> IO P7, switch_3
+		if (!lastSwitch_3){
+			lock_7 = true;
+			switch_3 = !switch_3;
+			//FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &extra_1, 1);
+		}
+		lastSwitch_3 = true;
+	} else {
+		if (lastSwitch_3){
+			lock_7 = false;
+		}
+		lastSwitch_3 = false;
 	}
 
 }
 
 FEB_Buttons_readyToDrive(){
-	if (!(data2 & (1<<0))){ //BuT_3 -> IO2 P0, ready to drive
+	if (!(data2 & (1<<0))){ //RTD -> IO P0, ready to drive
 		//buf_len = sprintf((char*)buf, "BUT_3\r\n");
 		//HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
 
@@ -128,7 +216,7 @@ FEB_Buttons_readyToDrive(){
 
 	//buf_len = sprintf((char*)buf, "check:%d flag:%d\r\n", Button_Checking, Button_Timer_Flag);
 	//HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
-	buf_len = sprintf((char*)buf, "ready: %d coolant: %d accumulator: %d extra: %d\r\n", ready_to_drive, coolant_pump, accumulator_fans, extra);
+	buf_len = sprintf((char*)buf, "ready: %d coolant: %d accumulator: %d extra_1: %d\r\n", ready_to_drive, coolant_pump, accumulator_fans, extra_1);
 	HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
 }
 
