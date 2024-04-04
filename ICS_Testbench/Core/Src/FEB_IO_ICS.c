111b8f3c14794c109b8398afc88de2b963be0c91
Raadwan Masum <piraadwan@gmail.com>
2024-04-03 22:32:35 -0700

    map all buttons and switches

--- ICS_Testbench/Core/Src/FEB_IO_ICS.c
+++ ICS_Testbench/Core/Src/FEB_IO_ICS.c
@@ -5,9 +5,10 @@
 extern I2C_HandleTypeDef hi2c1;
 
 static const uint16_t IOEXP_ADDR = 0x40;
+static const uint32_t BTN_HOLD_TIME = 2000;
 
 // **************************************** Functions ****************************************
-uint32_t rtdPressStartTime;
+uint32_t rtd_press_start_time;
 
 void FEB_IO_ICS_Init(void) {
 	uint8_t initial_io_exp_state = 0b11111110; // initialize RTD (P0) to low
@@ -20,56 +21,80 @@ void FEB_IO_ICS_Loop(void) {
 
 	HAL_I2C_Master_Receive(&hi2c1, IOEXP_ADDR, &received_data, 1, HAL_MAX_DELAY);
 
-//	uint8_t currButton = 0;
+	uint8_t button_state = ~received_data;
 
-	uint8_t rtdButtonState = 0;
-	uint8_t setRtdBuzzer = 0;
-	uint32_t rtdHoldTimeMs = 2000;
+	button_state = set_n_bit(button_state, 0, 0);
+	button_state = set_n_bit(button_state, 1, 0);
 
-	// Port 1 = Ready-to-Drive (RTD) button
+	uint8_t set_rtd_buzzer = 0;
+
+	// Button 1 = Ready-to-Drive (RTD) button
 	if (!(received_data & (1<<1))) {
-		rtdButtonState = 1;
-
-		if ((HAL_GetTick() - rtdPressStartTime) >= rtdHoldTimeMs) {
-			setRtdBuzzer = 1;
-			rtdButtonState = 0;
+		if ((HAL_GetTick() - rtd_press_start_time) >= BTN_HOLD_TIME) {
+			button_state = (uint8_t) set_n_bit(button_state, 1, 1);
+			set_rtd_buzzer = 1;
 		}
 	} else {
-		rtdButtonState = 0;
-		setRtdBuzzer = 0;
-		rtdPressStartTime = HAL_GetTick();
+		button_state = (uint8_t) set_n_bit(button_state, 1, 0);
+		rtd_press_start_time = HAL_GetTick();
+		set_rtd_buzzer = 0;
 	}
 
-	if (setRtdBuzzer == 1) {
-		lv_label_set_text(ui_buttonField, "RTD Button Held");
-	} else if (rtdButtonState == 1) {
-		lv_label_set_text(ui_buttonField, "RTD Button Pressed");
+	// Button 2
+	if (!(received_data & (1<<2))) {
+		button_state = (uint8_t) set_n_bit(button_state, 2, 1);
 	} else {
-		lv_label_set_text(ui_buttonField, "No Buttons Pressed");
+		button_state = (uint8_t) set_n_bit(button_state, 2, 0);
 	}
 
-//	if (!(received_data & (1<<2))) {
-//		currButton = 2;
-//	}
-//
-//	if (!(received_data & (1<<3))) {
-//		currButton = 3;
-//	}
-//
-//	if (!(received_data & (1<<4))) {
-//		currButton = 4;
-//	}
+	// Button 3
+	if (!(received_data & (1<<3))) {
+		button_state = (uint8_t) set_n_bit(button_state, 3, 1);
+	} else {
+		button_state = (uint8_t) set_n_bit(button_state, 3, 0);
+	}
 
-//	if (currButton != 0) {
-//		char buttonStr[16] = "Button X Pressed";
-//		char buttonNumStr[4];
-//
-//		sprintf(buttonNumStr, "%d", currButton);
-//
-//		buttonStr[7] = buttonNumStr[0];
-//
-//		lv_label_set_text(ui_buttonField, buttonStr);
-//	} else {
-//		lv_label_set_text(ui_buttonField, "No Buttons Pressed");
-//	}
+	// Button 4
+	if (!(received_data & (1<<4))) {
+		button_state = (uint8_t) set_n_bit(button_state, 4, 1);
+	} else {
+		button_state = (uint8_t) set_n_bit(button_state, 4, 0);
+	}
+
+	// Switch 1
+	if (!(received_data & (1<<7))) {
+		button_state = (uint8_t) set_n_bit(button_state, 5, 1);
+	} else {
+		button_state = (uint8_t) set_n_bit(button_state, 5, 0);
+	}
+
+	// Switch 2
+	if (!(received_data & (1<<5))) {
+		button_state = (uint8_t) set_n_bit(button_state, 6, 1);
+	} else {
+		button_state = (uint8_t) set_n_bit(button_state, 6, 0);
+	}
+
+	// Switch 3
+	if (!(received_data & (1<<6))) {
+		button_state = (uint8_t) set_n_bit(button_state, 7, 1);
+	} else {
+		button_state = (uint8_t) set_n_bit(button_state, 7, 0);
+	}
+
+	// display button state on UI
+	char button_state_str[9];
+	uint8_to_binary_string(button_state, button_state_str);
+	lv_label_set_text(ui_buttonField, button_state_str);
+}
+
+uint8_t set_n_bit(uint8_t x, uint8_t n, uint8_t bit_value) {
+	return (x & (~(1 << n))) | (bit_value << n);
+}
+
+void uint8_to_binary_string(uint8_t value, char *binary_string) {
+    for (int i = 7; i >= 0; i--) {
+    	binary_string[7 - i] = ((value >> i) & 1) ? '1' : '0';
+    }
+    binary_string[8] = '\0';
 }
