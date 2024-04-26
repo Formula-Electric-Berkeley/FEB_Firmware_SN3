ce070425436b3f916647edf915bc22a327587bdb
Raadwan Masum <piraadwan@gmail.com>
2024-04-26 16:53:39 -0700

    ICS is broken

---
+++ ICS/Core/Inc/FEB_UI.h
@@ -0,0 +1,26 @@
+#ifndef INC_FEB_UI_H_
+#define INC_FEB_UI_H_
+
+// **************************************** Includes ****************************************
+
+#include "FEB_CAN_ICS.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lvgl.h"
+#include "screen_driver.h"
+#include "touch_sensor_driver.h"
+#include "ui.h"
+
+#include "stm32f4xx_hal.h"
+
+// **************************************** Functions ****************************************
+
+void FEB_UI_Init(void);
+
+void FEB_UI_Update(void);
+
+void FEB_UI_Set_Values(void);
+
+#endif /* INC_FEB_UI_H_ */
