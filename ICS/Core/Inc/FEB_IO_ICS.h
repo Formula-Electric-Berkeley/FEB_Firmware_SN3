99006c669005883c037bf0155c104ad224345c14
Smit Malde <smit334@berkeley.edu>
2024-05-19 22:49:27 -0700

    Modified headerfile of FEB_IO_ICS to be consistent with source file

--- ICS/Core/Inc/FEB_IO_ICS.h
+++ ICS/Core/Inc/FEB_IO_ICS.h
@@ -13,8 +13,8 @@ void FEB_IO_ICS_Init(void);
 
 void FEB_IO_ICS_Loop(void);
 
-uint8_t set_n_bit(uint8_t num, uint8_t n, uint8_t bit_value);
+static uint8_t set_n_bit(uint8_t num, uint8_t n, uint8_t bit_value);
 
-void uint8_to_binary_string(uint8_t value, char *binary_string);
+static void uint8_to_binary_string(uint8_t value, char *binary_string);
 
 #endif /* INC_FEB_IO_ICS_H_ */
