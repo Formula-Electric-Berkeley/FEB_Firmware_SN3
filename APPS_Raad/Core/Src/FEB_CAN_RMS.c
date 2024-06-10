48555df66e51f8bdc7031288fc4614d365eba6ff
EvanSandoval <evansandoval@berkeley.edu>
2024-06-10 00:26:32 -0700

    added current limiting for brownout protection

--- APPS_Raad/Core/Src/FEB_CAN_RMS.c
+++ APPS_Raad/Core/Src/FEB_CAN_RMS.c
@@ -137,15 +137,54 @@ float FEB_CAN_RMS_getFilteredTorque_Regen(void){
 }
 
 
+// Essentially we want our voltage to never drop below 400 to be safe (~2.85V per cell) 
+// To keep 400 as our floor setpoint, we will derate our torque limit based on voltage
+// We will control this by derating our PEAK_CURRENT value. 
+// Based on data at 510 V, we see that the voltage drops about 62V when commanding 65 A of current
+// This means we will assume the pack resistance is about 1 Ohm
+// Note this will likely be steeper as we approach lower SOC, but these cell dynamics are hopefully
+// negligible due to the heavy current limiting and the ohmic losses of the accumulator
+// Linear interpolation between (460V, 60/60A) and (410V, 10/60A), where PEAK_CURRENT is 60A
+float FEB_get_peak_current_delimiter()
+{
+	float accumulator_voltage = (RMS_MESSAGE.HV_Bus_Voltage-50.0) / 10.0;
+	float estimated_voltage_drop_at_peak = PEAK_CURRENT;
+	float start_derating_voltage = 400.0 + PEAK_CURRENT; // Assume R_acc = 1ohm
+	// Note: Comments are based on start_derating_voltage = 460V and PEAK_CURRENT = 60
+
+	// Saturate outside of 460 and 410
+	if (accumulator_voltage > start_derating_voltage)
+	{
+		return 1;
+	}
+	if (accumulator_voltage <= 410)
+	{
+		return (10.0 / PEAK_CURRENT); // limit to only 10A 
+	}
+
+	// TODO: add a low-pass filter on this value if speed oscillates at high power
+
+	//   x0    y0            x1    y1
+	// (460V, 60/60A) and (410V, 10/60A)
+
+	//      m   = (        y_1           -              y_0)              / (x_1 -          x_0)
+	float slope = ((10.0 / PEAK_CURRENT) - (PEAK_CURRENT / PEAK_CURRENT)) / (410.0 - (start_derating_voltage));
+	//      y     =   m     (       x            -          x_0          ) + y_0
+	float derater = slope * (accumulator_voltage - start_derating_voltage) + 1.0;
+
+	return derater;   
+}
+
 float FEB_CAN_RMS_getMaxTorque(void){
 	float accumulator_voltage = min(INIT_VOLTAGE, (RMS_MESSAGE.HV_Bus_Voltage-50) / 10); // TODO: consider reading from IVT
 	float motor_speed = RMS_MESSAGE.Motor_Speed * RPM_TO_RAD_S;
+	float peak_current_limited = PEAK_CURRENT * FEB_get_peak_current_delimiter();
  	// If speed is less than 15, we should command max torque
   	// This catches divide by 0 errors and also negative speeds (which may create very high negative torque values)
 	if (motor_speed < 15) {
 		return MAX_TORQUE;
 	}
-	float maxTorque = min(MAX_TORQUE, (accumulator_voltage * PEAK_CURRENT) / motor_speed);
+	float maxTorque = min(MAX_TORQUE, (accumulator_voltage * peak_current_limited) / motor_speed);
 	return maxTorque;
 }
 
